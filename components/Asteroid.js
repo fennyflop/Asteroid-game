5eb1d904994eea14a92206b36a7829444e895a7e
Alex Semochkin <fennyflop@gmail.com>
2020-11-10 22:02:28 +0300

    work added

--- components/Asteroid.js
+++ components/Asteroid.js
@@ -1,103 +1,105 @@
 let comboCounter = 0;
 
 export default class Asteroid {
+  constructor(selector, missSound) {
+    this._selector = selector;
+    this._handleButtonClicks = this._handleButtonClicks.bind(this);
+    this._animationTime = 0;
+  }
 
-    constructor (selector) {
-        this._selector = selector;
-        this._handleButtonClicks = this._handleButtonClicks.bind(this);      
+  _getTemplate() {
+    const asteroidElement = document
+      .querySelector(this._selector)
+      .content.cloneNode(true);
+    return asteroidElement;
+  }
+
+  renderAsteroid() {
+    this._asteroidElement = this._getTemplate();
+    this._asteroid = this._asteroidElement.querySelector(".asteroid");
+    this._asteroidCircle = this._asteroidElement.querySelector(
+      ".asteroid__element"
+    );
+    this._asteroidIndicator = this._asteroidElement.querySelector(
+      ".asteroid__indicator"
+    );
+    this._asteroidMiss = this._asteroidElement.querySelector(".asteroid__miss");
+    this._setEventListeners();
+    this._getAsteroidPlace();
+
+    return this._asteroid;
+  }
+
+  _setEventListeners() {
+    // Adding a clicking listener.
+
+    this._asteroidCircle.addEventListener("click", () => {
+      this._removeAsteroid();
+      this._increaseCombo();
+    });
+
+    // Adding a button listener.
+
+    this._asteroidCircle.addEventListener("mouseenter", () => {
+      document.addEventListener("keydown", this._handleButtonClicks);
+    });
+
+    this._handleRescale();
+  }
+
+  _handleButtonClicks(evt) {
+    if (evt.key === "z" || evt.key === "x") {
+      this._removeAsteroid();
+      this._increaseCombo();
+      this._removeHandleButtonClicks();
     }
+  }
 
-    _getTemplate () {
-        const asteroidElement = document
-        .querySelector(this._selector)
-        .content
-        .cloneNode(true);
-        return asteroidElement;
-    }
+  _handleRescale() {
+    this._asteroidIndicator.addEventListener("animationend", () => {
+      this._handleMiss();
+      this._nulifyCombo();
+      this._removeHandleButtonClicks();
+    });
+  }
 
-    renderAsteroid () {
-        this._asteroidElement = this._getTemplate(); 
-        this._asteroid = this._asteroidElement.querySelector('.asteroid');
-        this._asteroidCircle = this._asteroidElement.querySelector('.asteroid__element');
-        this._asteroidIndicator = this._asteroidElement.querySelector('.asteroid__indicator');
-        this._asteroidMiss = this._asteroidElement.querySelector('.asteroid__miss');
-        this._setEventListeners();
-        this._getAsteroidPlace();
-        return this._asteroid;
-    }
+  _removeAsteroid() {
+    this._asteroid.remove();
+  }
 
-    _setEventListeners () {
-        // Adding a clicking listener.
+  _handleMiss() {
+    this._asteroid.style.visibility = "hidden";
+    this._asteroidIndicator.style.visibility = "hidden";
+    this._asteroidMiss.style.visibility = "visible";
+    setTimeout(() => {
+      this._removeAsteroid();
+    }, 700);
+  }
 
-        this._asteroidCircle.addEventListener('click', () => {
-            this._removeAsteroid();
-            this._increaseCombo();
-        });
+  _getAsteroidPlace() {
+    this._asteroid.style.top = `${this._getRandomInt(89)}vh`;
+    this._asteroid.style.left = `${this._getRandomInt(89)}vw`;
+  }
 
-        // Adding a button listener.
+  _getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 
-        this._asteroidCircle.addEventListener('mouseenter', () => {
-            document.addEventListener('keydown', this._handleButtonClicks);
-        });
+  _removeHandleButtonClicks() {
+    document.removeEventListener("keydown", this._handleButtonClicks);
+  }
 
-        // Handling the lose option
-        
-        this._handleRescale();
-    }
+  _increaseCombo() {
+    comboCounter += 1;
+    this._updateComboCounter();
+  }
 
-    _handleButtonClicks (evt) {
-        if(evt.key === 'z' || evt.key === 'x') {
-            this._removeAsteroid(); 
-            this._increaseCombo();
-            this._removeHandleButtonClicks();
-        }
-    }
+  _nulifyCombo() {
+    comboCounter = 0;
+    this._updateComboCounter();
+  }
 
-    _handleRescale () {
-        this._asteroidIndicator.addEventListener('animationend', () => {
-            this._handleMiss();
-            this._nulifyCombo();
-            this._removeHandleButtonClicks();
-        })
-    }
-
-    _removeAsteroid() {
-        this._asteroid.remove();
-    }
-
-    _handleMiss () {
-        this._asteroid.style.visibility = 'hidden';
-        this._asteroidIndicator.style.visibility = 'hidden';
-        this._asteroidMiss.style.visibility = 'visible';
-        setTimeout(() => {
-            this._removeAsteroid();
-        }, 700)
-    }
-
-    _getAsteroidPlace () {  
-        this._asteroid.style.top = `${this._getRandomInt(89)}vh`;
-        this._asteroid.style.left = `${this._getRandomInt(89)}vw`;
-    }
-
-    _getRandomInt (max) {
-        return Math.floor(Math.random() * Math.floor(max));
-    }
-
-    _removeHandleButtonClicks () {
-        document.removeEventListener('keydown', this._handleButtonClicks);
-    }
-
-    _increaseCombo () {
-        comboCounter += 1;
-        this._updateComboCounter();
-    }
-
-    _nulifyCombo () {
-        comboCounter = 0;
-        this._updateComboCounter();
-    }
-
-    _updateComboCounter () {
-        document.querySelector('.game__combo').textContent = `${comboCounter}x`;
-    }
+  _updateComboCounter() {
+    document.querySelector(".game__combo").textContent = `${comboCounter}x`;
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,86 @@
 import {
-    gameArea, asteroidTemplate, counter, endScreen,
-    restart, stats, settingsButton, settings,
-    returnButton, settingsFormElement, settingsRange,
-    endScreenTitle, buttons, hitSound, clickSound, loseSound,
-    colours,
-} from '../utils/constants.js';
+  gameArea,
+  asteroidTemplate,
+  counter,
+  endScreen,
+  restart,
+  stats,
+  settingsButton,
+  settings,
+  returnButton,
+  settingsFormElement,
+  settingsRange,
+  endScreenTitle,
+  buttons,
+  colours,
+} from "../utils/constants.js";
 
-import Asteroid from '../components/Asteroid.js';
+import Asteroid from "../components/Asteroid.js";
+
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function () {
+    this.sound.play();
+  };
+  this.stop = function () {
+    this.sound.pause();
+  };
+}
+
+const hitSound = new sound("./sounds/blop.mp3");
+const clickSound = new sound("./sounds/click.mp3");
+const missSound = new sound("./sounds/losing.mp3");
 
 const renderAsteroids = setInterval(() => {
-    const asteroid = new Asteroid ('#asteroid-template');
-    const asteroidElement = asteroid.renderAsteroid();
-    gameArea.prepend(asteroidElement);
+  const asteroid = new Asteroid("#asteroid-template");
+  const asteroidElement = asteroid.renderAsteroid();
+  gameArea.prepend(asteroidElement);
 }, 1500);
 
-// const asteroid = new Asteroid ('#asteroid-template');
-// const asteroidElement = asteroid.renderAsteroid();
-// gameArea.prepend(asteroidElement);
+const asteroid = new Asteroid("#asteroid-template");
+const asteroidElement = asteroid.renderAsteroid();
+gameArea.prepend(asteroidElement);
 
-function showDestroyedNum () {
-    counter.innerHTML = `Destroyed : ${destroyedNum} | <a class="copyright" href="https://github.com/fennyflop">fennyflop</a>`
+function showDestroyedNum() {
+  counter.innerHTML = `Destroyed : ${destroyedNum} | <a class="copyright" href="https://github.com/fennyflop">fennyflop</a>`;
 }
 
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        clickSound.play();
-    })
-})
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    clickSound.play();
+  });
+});
 
+settingsButton.addEventListener("click", () => {
+  settings.classList.add("screen_opened");
+});
 
-settingsButton.addEventListener('click', () => {
-    settings.classList.add('screen_opened');
-})
+returnButton.addEventListener("click", () => {
+  settings.classList.remove("screen_opened");
+  lifeSpan = currentLifespan;
+});
 
-returnButton.addEventListener('click', () => {
-    settings.classList.remove('screen_opened');
-    lifeSpan = currentLifespan;
-})
+restart.addEventListener("click", () => {
+  destroyedNum = 0;
+  gameArea.innerHTML = "";
+  counter.innerHTML = `Destroyed : ${destroyedNum} | <a class="copyright" href="https://github.com/fennyflop">fennyflop</a>`;
+  endScreen.classList.remove("screen_opened");
+  lifeSpan = currentLifespan;
+});
 
-restart.addEventListener('click', () => {
-    destroyedNum = 0;
-    gameArea.innerHTML = '';
-    counter.innerHTML = `Destroyed : ${destroyedNum} | <a class="copyright" href="https://github.com/fennyflop">fennyflop</a>`;
-    endScreen.classList.remove('screen_opened');
-    lifeSpan = currentLifespan;
-})
-
-function submitSettings (evt) {
-    evt.preventDefault();
-    currentLifespan = settingsRange.value;
-    settings.classList.remove('screen_opened');
-
+function submitSettings(evt) {
+  evt.preventDefault();
+  currentLifespan = settingsRange.value;
+  settings.classList.remove("screen_opened");
 }
 
-settingsFormElement.addEventListener('submit', submitSettings)
+settingsFormElement.addEventListener("submit", submitSettings);
+
+setInterval(() => {
+  missSound.play();
+}, 1000);

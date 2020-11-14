012d8659cebebedf200609dee3454b4512960363
Alex Semochkin <fennyflop@gmail.com>
2020-11-14 11:26:28 +0300

    mainscreen added

--- pages/index.js
+++ pages/index.js
@@ -23,19 +23,13 @@ const renderAsteroids = setInterval(() => {
   // gameArea.prepend(asteroidElement);
 }, 1500);
 
-const asteroid = new Asteroid("#asteroid-template");
-const asteroidElement = asteroid.renderAsteroid();
-gameArea.prepend(asteroidElement);
+// const asteroid = new Asteroid("#asteroid-template");
+// const asteroidElement = asteroid.renderAsteroid();
+// gameArea.prepend(asteroidElement);
 
 function showDestroyedNum() {
   counter.innerHTML = `Destroyed : ${destroyedNum} | <a class="copyright" href="https://github.com/fennyflop">fennyflop</a>`;
 }
-
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    clickSound.play();
-  });
-});
 
 settingsButton.addEventListener("click", () => {
   settings.classList.add("screen_opened");
